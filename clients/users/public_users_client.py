4aed194cb5b6a59c3c98d195e05ee8357f3ed367
AlinaKas24 <alina.kasperovich9@gmail.com>
2026-06-02 21:45:05 +0500

    task:HTTPX

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,30 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import ApiClient
+
+
+class CreateUserRequestDict(TypedDict):
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class PublicUsersClient(ApiClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        """
+            Метод выполняет создание пользователя.
+
+            :param request: Словарь с "email","password","lastName","firstName",
+        "middleName".
+            :return: Ответ от сервера в виде объекта httpx.Response
+        """
+
+        return self.post("/api/v1/users", json=request)
