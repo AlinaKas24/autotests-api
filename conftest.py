b6d24853e1853e4903e97289b4ce76174e00da26
AlinaKas24 <alina.kasperovich9@gmail.com>
2026-06-08 16:06:26 +0500

    API автотесты - Pytest(fixture)

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from pydantic import BaseModel
+
+from clients.authentication.authentication_client import (
+    AuthenticationClient,
+    get_authentication_client,
+)
+from clients.users.public_users_client import PublicUsersClient, get_public_users_client
+from models.pydantic_create_user_model import (
+    CreateUserRequestSchema,
+    CreateUserResponseSchema,
+)
+
+
+class UserFixture(BaseModel):
+    request: CreateUserRequestSchema
+    response: CreateUserResponseSchema
+
+    @property
+    def email(self) -> str:
+        return self.request.email
+
+    @property
+    def password(self) -> str:
+        return self.request.password
+
+
+@pytest.fixture
+def authentication_client() -> AuthenticationClient:
+    return get_authentication_client()
+
+
+@pytest.fixture
+def public_user_client() -> PublicUsersClient:
+    return get_public_users_client()
+
+
+@pytest.fixture
+def function_user(public_user_client: PublicUsersClient) -> UserFixture:
+    request = CreateUserRequestSchema()
+    response = public_user_client.create_user(request)
+    return UserFixture(request=request, response=response)
--- tests/test_user.py
+++ tests/test_user.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from models.pydantic_create_user_model import (
     CreateUserRequestSchema,
     CreateUserResponseSchema,
@@ -17,11 +17,9 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
-    public_users_client = get_public_users_client()
-
+def test_create_user(public_user_client:PublicUsersClient):
     request = CreateUserRequestSchema()
-    response = public_users_client.create_user_api(request)
+    response = public_user_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
